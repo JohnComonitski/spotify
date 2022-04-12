1414c366037537517c50e53cf8d246095338fe51
JohnComonitski <johncomonitski@gmail.com>
2022-04-12 11:05:58 -0400

    logs

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,15 +3,18 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req){
     const token = await getToken({ req, secret: process.env.JWT_SECRET });
-
+    console.log(token)
     const { pathname }  = req.nextUrl;
+    console.log(pathname)
 
     if(pathname.includes("/api/auth") || token){
+        console.log("going to next")
         return NextResponse.next()
     }
 
     if (!token && pathname !== "/login"){
-        return NextResponse.rewrite(new URL('/login', req.url))
-        //return NextResponse.redirect("/login")
+        console.log("going to login")
+        //return NextResponse.rewrite(new URL('/login', req.url))
+        return NextResponse.redirect("/login")
     }
 }
--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,7 @@ function Login({providers}){
     return (
         <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
             <h1 className="text-white font-bold text-lg pb-4">Welcome to the Spotify Network Visualizer</h1>
-            <img className="w-52 mb-5" src="http://links.papareact.com/9xl" alt="" />
+            <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
 
             {Object.values(providers).map((provider) =>(
                 <div key={provider.name}>
